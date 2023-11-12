71f1cc931b3ebc23f8c99489fb47bdd47ea175a3
limokipsang <limonoah1960@gamil.com>
2023-11-12 11:23:10 -0800

    task3

--- main.h
+++ main.h
@@ -11,6 +11,13 @@ int print_37(void);
 int print_int(va_list args);
 int print_dec(va_list args);
 int print_bin(va_list val);
+int print_unsigned(va_list args);
+int print_oct(va_list val);
+int print_hex(va_list val);
+int print_HEX(va_list val);
+
+
+
 
 
 
